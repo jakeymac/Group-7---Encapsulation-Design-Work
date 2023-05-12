e69898fc01da973004dbb6711454c1b655f81095
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-05-11 20:00:49 -0700

    added header comments

--- Week 4/Problem Set/containers.cpp
+++ Week 4/Problem Set/containers.cpp
@@ -1,14 +1,15 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Jacob Johnson and Jaden Myers
  * 2. Assignment Name:
  *      Practice 04: Contaiers
  * 3. Assignment Description:
  *      Work with a list and a vector
  * 4. What was the hardest part? Be as specific as possible.
- *      -a paragraph or two about how the assignment went for you-
+ *      Not bad, pretty straight forward. the most involved thing
+ *      was probably using the iterator and reverse iterator.
  * 5. How long did it take for you to complete the assignment?
- *      -total time in hours: reading the assignment, submitting, etc.
+ *      30 minutes
  **************************************************************/
 
 
