07892eda28c377b0cd4adfa9d88887a53a075186
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-05-30 17:03:13 -0600

    Create wallet.h

---
+++ Week 7/Practice/wallet.h
@@ -0,0 +1,24 @@
+/*************************************************************
+ * 1. Name:
+ *      -your name-
+ * 2. Module
+ *      WALLET
+ * 3. Assignment Description:
+ *      A class to represent an wallet with multiple credit cards
+ **************************************************************/
+
+#pragma once
+
+#include <cassert>
+
+class TestWallet;
+
+ /************************************
+  * WALLET
+  ************************************/
+class Wallet
+{
+   friend TestWallet;
+
+};
+
