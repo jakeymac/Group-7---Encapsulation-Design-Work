0ba05622b55428c1812c9996ea0bc159cb12bb03
Jacob Johnson <jakeymacaroni@gmail.com>
2023-06-03 13:10:02 -0600

    Added initial lab files

--- Week 7/Practice/wallet.h
+++ Week 7/Practice/wallet.h
@@ -1,6 +1,6 @@
 /*************************************************************
  * 1. Name:
- *      -your name-
+ *      Jacob and Jaden
  * 2. Module
  *      WALLET
  * 3. Assignment Description:
@@ -19,24 +19,30 @@ class TestWallet;
 class Wallet
 {
    friend TestWallet;
-private:
-    double accountSam;
-    double accountSue;
-    double * pCurrentAccount;
-    
-public:
-    Wallet();
-    void updateSam(double amount) {accountSam += amount;};
-    void updateSue(double amount) {accountSue += amount;};
-    void updateCurrent(double amount) {*pCurrentAccount += amount;};
-    void chooseSam() { pCurrentAccount = &accountSam; };
-    void chooseSue() { pCurrentAccount = &accountSue; };
-    void chooseCurrent();
-    double getSam() { return accountSam; };
-    double getSue() { return accountSue; };
-    double getCurrent() { return *pCurrentAccount; };
+    private:
+        double accountSam;
+        double accountSue;
+        double * pCurrentAccount;
+        
+    public:
+        Wallet() noexcept;
+        void updateSam(double amount) {accountSam += amount;};
+        void updateSue(double amount) {accountSue += amount;};
+        void updateCurrent(double amount) {*pCurrentAccount += amount;};
+        void chooseSam() { pCurrentAccount = &accountSam; };
+        void chooseSue() { pCurrentAccount = &accountSue; };
+        void chooseCurrent();
+        double getSam() { return accountSam; };
+        double getSue() { return accountSue; };
+        double getCurrent() { return *pCurrentAccount; };
     
 };
+
+Wallet::Wallet() noexcept {
+    accountSam = 0.0;
+    accountSue = 0.0;
+    pCurrentAccount = &accountSam;
+}
 
 void Wallet::chooseCurrent() {
     if (getSam() > getSue()) {
