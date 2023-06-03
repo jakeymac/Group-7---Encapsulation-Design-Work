355e269ae1c70e38143d78d6d96eda6b7bcfa760
Scorpio-555 <mye18006@byui.edu>
2023-06-03 16:15:48 -0600

    area of circle

--- Week 7/Lab/230-12/trigonometry.h
+++ Week 7/Lab/230-12/trigonometry.h
@@ -1,4 +1,5 @@
 #pragma once
+#define PI 3.14159265359
 
 #include <cmath>
 
@@ -11,5 +12,6 @@ public:
 	static double computeHorizontalComponent(double radians, double total) { return total * sin(radians); }
 	static double computeTotalComponent(double horizontalComponent, double verticalComponent) { return sqrt(pow(horizontalComponent, 2) + pow(verticalComponent, 2)); }
     static double distanceBetweenPoints(Position startPt, Position endPt) {return abs(sqrt(pow( (endPt.getMetersX() - startPt.getMetersX()), 2 ) + pow( (endPt.getMetersY() - startPt.getMetersY()), 2))); }
+	static double areaOfCircle(double radius) { return PI * radius * radius; }
     
 };
