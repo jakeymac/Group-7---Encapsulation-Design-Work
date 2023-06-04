df22929fe56609b3a3053c19fbe1a627413006d2
Scorpio-555 <mye18006@byui.edu>
2023-06-03 19:28:11 -0600

    Update trigonometry.h

--- Week 7/Lab/230-12/trigonometry.h
+++ Week 7/Lab/230-12/trigonometry.h
@@ -13,5 +13,6 @@ public:
 	static double computeTotalComponent(double horizontalComponent, double verticalComponent) { return sqrt(pow(horizontalComponent, 2) + pow(verticalComponent, 2)); }
     static double distanceBetweenPoints(Position startPt, Position endPt) {return abs(sqrt(pow( (endPt.getMetersX() - startPt.getMetersX()), 2 ) + pow( (endPt.getMetersY() - startPt.getMetersY()), 2))); }
 	static double areaOfCircle(double radius) { return PI * radius * radius; }
+	static double getAngle(double dx, double dy) { return atan2(dy, dx); }
     
 };
