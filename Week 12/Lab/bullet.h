f0cdf28477b9b890308c2383f3069a74f32486ad
Scorpio-555 <78047645+Scorpio-555@users.noreply.github.com>
2023-07-07 09:01:11 -0600

    Create bullet.h

---
+++ Week 12/Lab/bullet.h
@@ -0,0 +1,38 @@
+/***********************************************************************
+ * Header File:
+ *    Class : brief definition
+ * Author:
+ *    add author
+ * Summary:
+ *    add
+ *    summary
+ ************************************************************************/
+
+#pragma once
+
+#include "position.h"
+#include "ground.h"
+#include "physics.h"
+#include <list>
+#include <iterator>
+
+using namespace std;
+
+/*********************************************
+ * Class
+ * brief definition
+ *********************************************/
+class Bullet {
+public:
+    Bullet(Position p, double radians, Ground * ground);
+    bool isAlive() const;
+    void advance();
+    list<Position> getStreakPath() const;
+    Position getImpactPosition() { return impactPos; }
+    bool hitGround();
+    
+private:
+    list<Position> streakPath;
+    Physics physics;
+    Position impactPos;
+};
